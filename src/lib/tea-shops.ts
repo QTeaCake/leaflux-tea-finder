caa314e47bb781db746323adc665bf7a294a4b3c
Anastatius Mayo <mayo.anastatius@gmail.com>
2026-02-12 07:45:01 +0000

    TAIAN 219 W Cermak Rd, Chicago, IL 60616 https://www.taianchicago.com/ +

--- src/lib/tea-shops.ts
+++ src/lib/tea-shops.ts
@@ -121,4 +121,94 @@ export const teaShops: TeaShop[] = [
       website: 'https://www.bluelanterntea.com/menu/',
     },
   },
+  {
+    id: 'taian-chicago',
+    name: 'TAIAN',
+    address: '219 W Cermak Rd, Chicago, IL 60616',
+    location: {
+      lat: 41.92830363239487,
+      lng: -87.64653838176316,
+    },
+    offerings: ['loose leaf'],
+    ethical: false,
+    contact: {
+      phone: '+17738882918',
+      website: 'https://www.taianchicago.com/',
+    },
+  },
+  {
+    id: 'japan-house-urbana',
+    name: 'Japan House, University of Illinois at Urbana-Champaign',
+    address: '2000 S Lincoln Ave, Urbana, IL 61802',
+    location: {
+      lat: 40.16362462868195,
+      lng: -88.21782742739538,
+    },
+    offerings: ['loose leaf', 'teaware', 'classes'],
+    ethical: false,
+    contact: {
+      phone: '+12172449934',
+      website: 'http://japanhouse.illinois.edu/',
+    },
+  },
+  {
+    id: 'ms-bs-teas-clayton',
+    name: "Ms B's Teas",
+    address: '4883 Iowa St, Clayton, IN 46118',
+    location: {
+      lat: 39.76787545895561,
+      lng: -86.50396029049867,
+    },
+    offerings: ['loose leaf'],
+    ethical: false,
+    contact: {
+      phone: '+13175392507',
+      website: 'http://www.msbsteas.com/menu',
+    },
+  },
+  {
+    id: 'tiger-spirit-teahouse-erie',
+    name: 'Tiger Spirit Teahouse',
+    address: '2131 W 8th St, Erie, PA 16505',
+    location: {
+      lat: 42.158120297956444,
+      lng: -80.15652325193659,
+    },
+    offerings: ['loose leaf'],
+    ethical: false,
+    contact: {
+      phone: '+18144800844',
+      website: 'https://tiger-spirit-teahouse.square.site/',
+    },
+  },
+  {
+    id: 'chengdu-teahouse-lansing',
+    name: 'Chengdu Teahouse',
+    address: '100 E César E. Chávez Ave, Lansing, MI 48906',
+    location: {
+      lat: 42.77723985365868,
+      lng: -84.53696385850519,
+    },
+    offerings: ['loose leaf'],
+    ethical: false,
+    contact: {
+      phone: '+19195250594',
+      website: 'https://www.thechengduteahouse.com/',
+    },
+  },
+  {
+    id: 'wonderful-matcha-la-grange',
+    name: 'Wonderful Matcha',
+    address: '4 W Burlington Ave, La Grange, IL 60525',
+    location: {
+      lat: 41.87503565442297,
+      lng: -87.88376538176318,
+    },
+    offerings: ['loose leaf'],
+    ethical: false,
+    contact: {
+      phone: '+17089379225',
+      website: 'http://wonderfulmatcha.com/',
+    },
+  },
 ];
