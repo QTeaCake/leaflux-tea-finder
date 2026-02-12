07572bd6012fc6c0596bb6671a14aab2ab2faea9
Anastatius Mayo <mayo.anastatius@gmail.com>
2026-02-12 04:16:35 +0000

    The Spice Shack 11334 Page Rd, Grabill, IN 46741 41.19440066348785, -84.

--- src/lib/tea-shops.ts
+++ src/lib/tea-shops.ts
@@ -15,4 +15,20 @@ export type TeaShop = {
   distance?: number;
 };
 
-export const teaShops: TeaShop[] = [];
+export const teaShops: TeaShop[] = [
+  {
+    id: 'the-spice-shack-grabill',
+    name: 'The Spice Shack',
+    address: '11334 Page Rd, Grabill, IN 46741',
+    location: {
+      lat: 41.19440066348785,
+      lng: -84.97670994071623,
+    },
+    offerings: ['loose leaf'],
+    ethical: false,
+    contact: {
+      phone: '+12606159634',
+      website: 'http://thespiceshack.org/',
+    },
+  },
+];
