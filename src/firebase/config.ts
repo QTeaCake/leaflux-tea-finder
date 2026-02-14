235dd711649fb221697ea5d72fb170942862845b
Anastatius Mayo <mayo.anastatius@gmail.com>
2026-02-14 05:58:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8763188321-d5a29",
+  "appId": "1:166922840606:web:c3f0e7643a5da439d01c0f",
+  "apiKey": "AIzaSyDFN7SFsQBYdohTEoVIr77c3QPXfNUV1rk",
+  "authDomain": "studio-8763188321-d5a29.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "166922840606"
+};
